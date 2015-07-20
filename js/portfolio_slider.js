ec71e05bb4a7db55879163d4bf79d4dc31c19e35
Rabimba Karanjai <rkaranj@Rabimbas-MacBook-Pro.local>
2015-07-19 23:17:53 -0700

    Initial Commit

---
+++ js/portfolio_slider.js
@@ -0,0 +1,61 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'Deepotsav',
+			'description'   : 'We at RICE try to make Deepotsav as large as possible for us. And you get a glopse of what we had in past here.',
+			'thumbnail' : ['img/portfolio/small/1.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/11.jpg'],
+			'large' : ['img/portfolio/large/1.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large/11.jpg'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['All']
+			},
+
+						{
+			'title' : 'Deepotsav',
+			'description'   : 'We at RICE try to make Deepotsav as large as possible for us. And you get a glopse of what we had in past here.',
+			'thumbnail' : ['img/portfolio/small/1.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/11.jpg'],
+			'large' : ['img/portfolio/large/1.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large/11.jpg'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['All']
+			},
+
+						{
+			'title' : 'Deepotsav',
+			'description'   : 'We at RICE try to make Deepotsav as large as possible for us. And you get a glopse of what we had in past here.',
+			'thumbnail' : ['img/portfolio/small/1.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/11.jpg'],
+			'large' : ['img/portfolio/large/1.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large/11.jpg'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['All']
+			},
+
+						{
+			'title' : 'Deepotsav',
+			'description'   : 'We at RICE try to make Deepotsav as large as possible for us. And you get a glopse of what we had in past here.',
+			'thumbnail' : ['img/portfolio/small/1.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/11.jpg'],
+			'large' : ['img/portfolio/large/1.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large/11.jpg'],
+			'button_list'   :
+			[
+			
+			],
+			'tags'  : ['All']
+			},
+
+			
+		]
+	});
+});
